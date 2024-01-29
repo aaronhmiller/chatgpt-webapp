62e955249e0ac978f2daa73a13935b2256c3f854
Aaron Miller <aaronmiller@gmail.com>
2024-01-28 16:14:56 -0800

    added create and tidying up

--- app.py
+++ app.py
@@ -14,9 +14,13 @@ db_params = {
 }
 
 # Define your REST API routes
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.route('/add')
+def create():
+    return render_template('add.html')
 
 @app.route('/users', methods=['GET'])
 def get_data():
@@ -56,6 +60,30 @@ def update_data(data_id):
 
     # Return updated data
     return jsonify({'message': 'Data updated successfully'})
+
+# Define the route for creating data
+@app.route('/users', methods=['POST'])
+def create_data():
+    # Connect to PostgreSQL database
+    connection = psycopg2.connect(**db_params)
+    cursor = connection.cursor()
+
+    # Get data from the request
+    data = request.json
+    new_name = data.get('newName')
+    new_email = data.get('newEmail')
+
+    # Update data in the database
+    cursor.execute('INSERT INTO users (name, email) VALUES (%s, %s) RETURNING id', (new_name, new_email))
+    new_data_id = cursor.fetchone()[0]
+    connection.commit()
+
+    # Close database connection
+    cursor.close()
+    connection.close()
+
+    # Return created data
+    return jsonify({'id': new_data_id, 'name': new_name, 'email': new_email})
 
 if __name__ == '__main__':
     app.run(debug=True)
